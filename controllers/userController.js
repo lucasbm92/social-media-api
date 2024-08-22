747bbfdab8e7362eeb57cae6c296f0448953c057
Lucas <lucas_bm92@hotmail.com>
2024-08-22 12:56:21 -0300

    Alterações.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,47 +1,73 @@
-// controllers/userController.js
 const bcrypt = require("bcrypt");
-const User = require("../models/User"); // Ajuste o caminho conforme necessário
+const conectarDB = require("../database"); // Ajuste o caminho conforme necessário
 
 exports.signup = async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = new User({
-      usuario: req.body.usuario,
-      email: req.body.email,
-      password: hashedPassword,
+    const { nome, email, senha } = req.body;
+
+    if (!nome || !email || !senha) {
+      return res
+        .status(400)
+        .send({ mensagem: "Todos os campos são obrigatórios" });
+    }
+
+    // Gera um salt e hash para a senha
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(senha, salt);
+
+    const db = await conectarDB();
+    const collection = db.collection("users");
+
+    const novoUsuario = {
+      nome,
+      email,
+      senha: hashedPassword,
+      dataCriacao: new Date(),
+    };
+
+    const resultado = await collection.insertOne(novoUsuario);
+    const usuarioCriado = await collection.findOne({
+      _id: resultado.insertedId,
     });
-    const newUser = await user.save();
-    res.status(201).json({ message: "Usuário criado com sucesso!", newUser });
+
+    res.status(201).send({
+      mensagem: "Usuário criado com sucesso",
+      usuario: usuarioCriado,
+    });
   } catch (error) {
     res
       .status(500)
-      .json({ message: "Erro ao criar usuário", error: error.message });
+      .send({ mensagem: "Erro ao criar usuário", error: error.message });
   }
 };
 
-// controllers/userController.js
-const jwt = require("jsonwebtoken");
-
 exports.login = async (req, res) => {
   try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(401).json({ message: "Autenticação falhou" });
+    const { email, senha } = req.body;
+
+    if (!email || !senha) {
+      return res
+        .status(400)
+        .send({ mensagem: "Todos os campos são obrigatórios" });
     }
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    if (!validPassword) {
-      return res.status(401).json({ message: "Autenticação falhou" });
+
+    const db = await conectarDB();
+    const collection = db.collection("users");
+
+    const usuario = await collection.findOne({ email });
+    if (!usuario) {
+      return res.status(400).send({ mensagem: "Usuário não encontrado" });
     }
-    const token = jwt.sign(
-      { userId: user._id, email: user.email },
-      "SEGREDO", // Use uma chave secreta real aqui
-      { expiresIn: "1h" }
-    );
-    res.status(200).json({ message: "Autenticado com sucesso", token });
+
+    const senhaValida = await bcrypt.compare(senha, usuario.senha);
+    if (!senhaValida) {
+      return res.status(400).send({ mensagem: "Senha inválida" });
+    }
+
+    res.status(200).send({ mensagem: "Login bem-sucedido" });
   } catch (error) {
-    res.status(500).json({ message: "Erro interno", error: error.message });
+    res
+      .status(500)
+      .send({ mensagem: "Erro ao fazer login", error: error.message });
   }
 };
