919482a8ff8aab580dc0e25fa08c60911012a4cf
Lucas <lucas_bm92@hotmail.com>
2024-08-23 12:37:20 -0300

    Incluída lógica de likes e comentários.

--- controllers/postController.js
+++ controllers/postController.js
@@ -57,3 +57,92 @@ exports.deletarPostagem = async (req, res) => {
       .send({ mensagem: "Erro ao deletar postagem", error: error.message });
   }
 };
+
+exports.likePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { userId } = req.body;
+
+    const db = await conectarDB();
+    const collection = db.collection("posts");
+
+    await collection.updateOne(
+      { _id: new ObjectId(postId) },
+      { $addToSet: { likes: userId } }
+    );
+
+    res.status(200).send({ mensagem: "Post curtido com sucesso" });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ mensagem: "Erro ao curtir post", error: error.message });
+  }
+};
+
+exports.unlikePost = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { userId } = req.body;
+
+    const db = await conectarDB();
+    const collection = db.collection("posts");
+
+    await collection.updateOne(
+      { _id: new ObjectId(postId) },
+      { $pull: { likes: userId } }
+    );
+
+    res.status(200).send({ mensagem: "Post descurtido com sucesso" });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ mensagem: "Erro ao descurtir post", error: error.message });
+  }
+};
+
+exports.addComment = async (req, res) => {
+  try {
+    const { postId } = req.params;
+    const { userId, comentario } = req.body;
+
+    const db = await conectarDB();
+    const collection = db.collection("posts");
+
+    const novoComentario = {
+      userId,
+      comentario,
+      dataComentario: new Date(),
+    };
+
+    await collection.updateOne(
+      { _id: new ObjectId(postId) },
+      { $push: { comments: novoComentario } }
+    );
+
+    res.status(200).send({ mensagem: "Comentário adicionado com sucesso" });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ mensagem: "Erro ao adicionar comentário", error: error.message });
+  }
+};
+
+exports.removeComment = async (req, res) => {
+  try {
+    const { postId, commentId } = req.params;
+
+    const db = await conectarDB();
+    const collection = db.collection("posts");
+
+    await collection.updateOne(
+      { _id: new ObjectId(postId) },
+      { $pull: { comments: { _id: new ObjectId(commentId) } } }
+    );
+
+    res.status(200).send({ mensagem: "Comentário removido com sucesso" });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ mensagem: "Erro ao remover comentário", error: error.message });
+  }
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,5 +6,12 @@ router.post("/", postController.criarPostagem);
 router.get("/", postController.lerPostagens);
 router.put("/:id", postController.atualizarPostagem);
 router.delete("/:id", postController.deletarPostagem);
+router.post("/posts/:postId/like", userController.likePost);
+router.post("/posts/:postId/unlike", userController.unlikePost);
+router.post("/posts/:postId/comments", userController.addComment);
+router.delete(
+  "/posts/:postId/comments/:commentId",
+  userController.removeComment
+);
 
 module.exports = router;
